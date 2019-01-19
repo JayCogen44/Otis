4191ccd8e1511d7de025bf0420711546e1e659af
JayCogen44 <jaycog44@gmail.com>
2019-01-19 13:55:09 -0800

    updates to the doc nav and main nav

--- client/components/DocNav.js
+++ client/components/DocNav.js
@@ -2,48 +2,33 @@ import React, { Component } from 'react';
 // import './css/DocNav.css';
 
 class DocNav extends Component {
-
-
-    render(){
-        
-        // ALLOWS ANIMATION OF DROPDOWN NAVIGATION
-        let coll = document.getElementsByClassName("collapsible");
-        let i;
-        
-        for (i = 0; i < coll.length; i++) {
-          coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight){
-              content.style.maxHeight = null;
-            } else {
-              content.style.maxHeight = content.scrollHeight + "px";
-            } 
-          });
+    constructor(props) {
+        super(props);
+        this.state = {
+            showDropdown: 0
         }
+    }
 
-        return(
+    render() {
+        const nav = [];
+        this.props.docData.forEach((e, i) => {
+            nav.push(
+                <div className={`dropdown ${i === this.state.showDropdown ? 'active' : ''}`}
+                    onClick={() => this.setState({ showDropdown: i })}
+                    key={i + Math.random()}
+                >
+                    <span className='collapsible'>{e.category}</span>
+                    <div className="content">
+                        <p>{e.title}</p>
+                    </div>
+                </div>
+            )
+        });
+
+        return (
             <div className='DocNav'>
-            <span className='collapsible'>DOC NAVIGATION </span>
-                <div className="content">
-                    <p>Item 1</p>
-                    <p>Item 2</p>
-                    <p>Item 3</p>
-                </div>
 
-            <span className='collapsible'>DOC NAVIGATION 2</span>
-                <div className="content">
-                    <p>Item 1</p>
-                    <p>Item 2</p>
-                    <p>Item 3</p>
-                </div>
-            
-            <span className='collapsible'>DOC NAVIGATION 3</span>
-                <div className="content">
-                    <p>Item 1</p>
-                    <p>Item 2</p>
-                    <p>Item 3</p>
-                </div>
+                {nav}
 
             </div>
         )
