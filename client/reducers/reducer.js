1d89f75eba6a64787cd35932ec5d86b2daafdb64
JayCogen44 <jaycog44@gmail.com>
2019-01-19 14:24:43 -0800

    got rid of console logs

--- client/reducers/reducer.js
+++ client/reducers/reducer.js
@@ -18,10 +18,6 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case types.GET_DOC:
-            console.log("This is action.payload: ", action.payload);
-            console.log("The content is found at action.payload at index 0: ", action.payload[0].content);
-            console.log("Action payload type: ", typeof action.payload)
-            console.log("Is action payload an array? ", Array.isArray(action.payload))
             return {
                 ...state,
                 docData: action.payload
